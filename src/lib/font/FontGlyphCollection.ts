f88e9558d0ef87aed0983463a3f0e1959e58a552
Imperger <mr.imperger@hotmail.com>
2023-11-12 12:55:58 +0000

    feat: Font glyph mesh build helper

---
+++ src/lib/font/FontGlyphCollection.ts
@@ -0,0 +1,156 @@
+import { NotNull } from "../NotNull";
+import { Vec2 } from "../Primitives";
+import { UV } from "../renderer/TextureAtlas";
+
+import { FontAtlas } from "./FontAtlasBuilder";
+
+import { AppResource } from "@/app/AppResource";
+import { Inversify } from "@/Inversify";
+
+interface GlyphMeshFontOptions {
+    Name: string;
+    Size: number;
+}
+
+interface GlyphMeshOptions {
+    ASCIIRange: { Start: string, End: string },
+    Font: GlyphMeshFontOptions
+}
+
+interface GlyphMeshBlueprint {
+    uv: UV; // exact glyph bound
+    width: number;
+    height: number;
+    baselineOffset: Vec2;
+}
+
+interface AtlasCellDescriptor {
+    uv: UV;
+    sideLength: number;
+}
+
+interface GlyphBoundary {
+    width: number;
+    height: number;
+    actualHeight: number;
+    fontBoundingBoxDescent: number;
+    actualBoundingBoxDescent: number;
+}
+
+export class FontGlyphCollection {
+    constructor(private library: Map<string, GlyphMeshBlueprint>) { }
+
+    Lookup(symbol: string): GlyphMeshBlueprint {
+        if (symbol.length === 0) {
+            throw new Error("Symbol can't be empty string");
+        }
+
+        const blueprint = this.library.get(symbol);
+
+        if (blueprint === undefined) {
+            throw new Error(`Failed to find glyph '${symbol}'`);
+        }
+
+        return blueprint;
+    }
+}
+
+class FontGlyphCollectionBuilderImpl {
+    private options!: GlyphMeshOptions;
+    private atlas!: FontAtlas;
+    private context!: CanvasRenderingContext2D;
+
+    private lib = new Map<string, GlyphMeshBlueprint>();
+
+    Build(options: GlyphMeshOptions): FontGlyphCollection {
+        this.options = options;
+        this.atlas = Inversify.get(AppResource).FontAtlas;
+
+        const startCode = options.ASCIIRange.Start.charCodeAt(0);
+        const endCode = options.ASCIIRange.End.charCodeAt(0);
+
+        if (startCode > endCode) {
+            throw new Error('options.ASCIIRange.Start symbol must precede options.ASCIIRange.End');
+        }
+
+
+        const sideLength = this.GlyphCellSize();
+        this.SetupCanvas(sideLength, sideLength);
+
+        this.BuildBlueprints(endCode - startCode + 1);
+
+        return new FontGlyphCollection(this.lib);
+    }
+
+    private SetupCanvas(width: number, height: number): void {
+        const canvas = document.createElement('canvas');
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const context = canvas.getContext('2d') ?? NotNull('Failed to get context');
+        context.font = `${this.options.Font.Size}px ${this.options.Font.Name}`;
+        context.fillStyle = '#000';
+
+        this.context = context;
+    }
+
+    private GlyphCellSize(): number {
+        return Math.ceil(this.options.Font.Size / 10) * 10;
+    }
+
+    private BuildBlueprints(glyphCount: number) {
+        for (let n = 0; n < glyphCount; ++n) {
+            const symbol = String.fromCharCode(this.options.ASCIIRange.Start.charCodeAt(0) + n);
+            const atlasUV = this.atlas.LookupUV(symbol);
+            const metrics = this.context.measureText(symbol);
+
+            const width = metrics.width;
+            const height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+            const actualHeight = metrics.actualBoundingBoxDescent + metrics.actualBoundingBoxAscent;
+
+            const uv = this.AtlasCellToGlyphUV(
+                { uv: atlasUV, sideLength: this.GlyphCellSize() },
+                { width, height, actualHeight, fontBoundingBoxDescent: metrics.fontBoundingBoxDescent, actualBoundingBoxDescent: metrics.actualBoundingBoxDescent });
+
+            const baselineOffset = {
+                x: 0,
+                y: metrics.fontBoundingBoxDescent - metrics.actualBoundingBoxDescent
+            };
+
+            this.lib.set(symbol, { uv, width, height, baselineOffset });
+        }
+    }
+
+    private AtlasCellToGlyphUV(cell: AtlasCellDescriptor, glyph: GlyphBoundary): UV {
+        const relA: Vec2 = {
+            x: (cell.sideLength - glyph.width) / 2,
+            y: (cell.sideLength - glyph.height) / 2
+        };
+
+        const relB: Vec2 = { x: relA.x + glyph.width, y: relA.y + glyph.height };
+
+        const uvWidth = cell.uv.B.x - cell.uv.A.x;
+        const uvHeight = cell.uv.B.y - cell.uv.A.y;
+
+        return {
+            A: {
+                x: cell.uv.A.x + relA.x / cell.sideLength * uvWidth,
+                y: cell.uv.A.y + relA.y / cell.sideLength * uvHeight
+            },
+            B: {
+                x: cell.uv.A.x + relB.x / cell.sideLength * uvWidth,
+                y: cell.uv.A.y + relB.y / cell.sideLength * uvHeight
+            }
+        };
+    }
+}
+
+export class FontGlyphCollectionBuilder {
+    private constructor() { }
+
+    static Build(options: GlyphMeshOptions): FontGlyphCollection {
+        return new FontGlyphCollectionBuilderImpl().Build(options);
+    }
+}
