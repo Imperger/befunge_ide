7210b9490216c2ddfaffcbfb4af5b1d812ec9f85
Imperger <mr.imperger@hotmail.com>
2023-11-12 12:38:25 +0000

    feat: More settings

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -2,6 +2,11 @@ import { injectable } from "inversify";
 
 import { Inversify } from "@/Inversify";
 
+interface Dimension {
+    Width: number;
+    Height: number;
+}
+
 @injectable()
 export class AppSettings {
     public readonly ZNear = 1;
@@ -11,6 +16,12 @@ export class AppSettings {
     public readonly Fovy = 60 / 180 * Math.PI;
 
     public AspectRatio = 1;
+
+    public ViewDimension: Dimension = { Width: 800, Height: 600 };
+
+    public ExecutionTimeout = 1000;
+
+    public MaxOutputLength = 1000;
 }
 
 Inversify.bind(AppSettings).toSelf().inSingletonScope();
