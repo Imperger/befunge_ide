0a02308666c48ebb330b5b7b70d17a4972b20dee
Imperger <mr.imperger@hotmail.com>
2023-11-28 10:46:13 +0000

    feat: Relax conditions for click event appearance

--- src/app/AppEventTransformer.ts
+++ src/app/AppEventTransformer.ts
@@ -1,25 +1,58 @@
+import { vec2 } from "gl-matrix";
 import { injectable } from "inversify";
 
 import { MouseButtons } from "@/lib/DOM/MouseButtons";
+import { Vec2 } from "@/lib/Primitives";
+
+class OnSelectStrategy {
+    private mouseDownStart = 0;
+    private lastMouseDownPosition: Vec2 = { x: 0, y: 0 };
+
+    private isSelect = false;
+
+    constructor(
+        private releaseTimeout: number,
+        private maxDistance: number) { }
+
+    OnMouseDown(e: MouseEvent): void {
+        this.mouseDownStart = Date.now();
+        this.lastMouseDownPosition.x = e.offsetX;
+        this.lastMouseDownPosition.y = e.offsetY;
+    }
+
+    OnMouseUp(e: MouseEvent): void {
+        const distance = vec2.dist(
+            [this.lastMouseDownPosition.x, this.lastMouseDownPosition.y],
+            [e.offsetX, e.offsetY]);
+
+        const elapsedTime = Date.now() - this.mouseDownStart;
+
+        this.isSelect = distance <= this.maxDistance && elapsedTime <= this.releaseTimeout;
+    }
+
+    get IsSelect(): boolean {
+        return this.isSelect;
+    }
+}
 
 @injectable()
 export abstract class AppEventTransformer {
-    private isMovement = false;
+    private readonly selectStrategy = new OnSelectStrategy(250, 3);
 
     OnMouseMove(e: MouseEvent): void {
         if (e.buttons & MouseButtons.Left) {
-            this.isMovement = true;
-
             this.OnCameraMove(e);
         }
     }
 
-    OnMouseDown(_e: MouseEvent): void {
-        this.isMovement = false;
+    OnMouseDown(e: MouseEvent): void {
+        this.selectStrategy.OnMouseDown(e);
     }
 
     OnMouseUp(e: MouseEvent): void {
-        if (!this.isMovement) {
+        this.selectStrategy.OnMouseUp(e);
+
+        if (this.selectStrategy.IsSelect) {
             this.OnSelect(e);
         }
     }
