77cf4ea7610b65aef1bd96d871ca9fb1ab3f0d0a
Imperger <mr.imperger@hotmail.com>
2024-02-08 09:19:07 +0000

    chore : Deploy script

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import store from './store';
 
 async function Main(): Promise<void> {
     // Explicitly load the font otherwise it not be visible for the background canvas that used for generating a bitmap font
-    const roboto = new FontFace('Roboto', 'url(fonts/roboto-latin-500-normal.3170fd9a.woff2)');
+    const roboto = new FontFace('Roboto', `url(${process.env.BASE_URL}fonts/roboto-latin-500-normal.3170fd9a.woff2)`);
     await roboto.load();
 
     createApp(App).use(store).use(router).mount('#app');
