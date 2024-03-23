d31fc6ae91319f8e7a4207c61f8c1a526a503e96
Imperger <mr.imperger@hotmail.com>
2024-03-23 11:45:27 +0000

    feat: Style language syntax manual

--- src/app/Overlay/LanguageSyntaxHelpControls.ts
+++ src/app/Overlay/LanguageSyntaxHelpControls.ts
@@ -13,13 +13,24 @@ import { HorizontalAnchor, UIObservablePositioningGroup, VerticalAnchor } from "
 import { UIRenderer } from "@/lib/UI/UIRenderer";
 import { UITextList } from "@/lib/UI/UITextList/UITextList";
 
+interface AutoWordWrapResult {
+    text: string;
+    instructionRange: [number, number][];
+}
+
+interface InstructionDescription {
+    instruction: string;
+    description: string;
+}
+
 @injectable()
 export class LanguageSyntaxHelpControls {
     private group: UIObservablePositioningGroup;
     private display: UITextList | null = null;
-    private lineHeight = 24;
+    private lineHeight = 16;
     private maxWidth = 1024;
     private defaultTextColor: Rgb = [0.9254901960784314, 0.9411764705882353, 0.9450980392156862];
+    private instructions: InstructionDescription[];
     private helpString!: string;
     private instructionPaddingEnd!: number;
 
@@ -31,11 +42,7 @@ export class LanguageSyntaxHelpControls {
             { x: 0, y: 0 },
             { vertical: VerticalAnchor.Bottom, horizontal: HorizontalAnchor.Middle });
 
-        this.BuildHelpString();
-    }
-
-    private BuildHelpString(): void {
-        const instructions = [
+        this.instructions = [
             { instruction: '0-9', description: 'Push this number onto the stack.' },
             { instruction: '+', description: 'Addition: Pop a and b, then push a+b.' },
             { instruction: '-', description: 'Subtraction: Pop a and b, then push b-a.' },
@@ -58,7 +65,7 @@ export class LanguageSyntaxHelpControls {
             { instruction: '.', description: 'Pop value and output as an integer followed by a space.' },
             { instruction: ',', description: 'Pop value and output as ASCII character.' },
             { instruction: '#', description: 'Bridge: Skip next cell.' },
-            { instruction: 'p', description: 'A "put" call (a way to store a value for later use). Pop y, x, and v, then change the character at (x,y) in the program to the character with ASCII value v' },
+            { instruction: 'p', description: 'A "put" call (a way to store a value for later use). Pop y, x, and v, then change the character at (x,y) in the program to the character with ASCII value v.' },
             { instruction: 'g', description: 'A "get" call (a way to retrieve data in storage). Pop y and x, then push ASCII value of the character at that position in the program.' },
             { instruction: '&', description: 'Ask user for a number and push it.' },
             { instruction: '~', description: 'Ask user for a character and push its ASCII value.' },
@@ -66,16 +73,20 @@ export class LanguageSyntaxHelpControls {
             { instruction: '" "', description: 'No-op. Does nothing' }
         ];
 
+        this.BuildHelpString();
+    }
+
+    private BuildHelpString(): void {
         this.instructionPaddingEnd = ArrayHelper
-            .Max(instructions, (a, b) => a.instruction.length < b.instruction.length)
+            .Max(this.instructions, (a, b) => a.instruction.length < b.instruction.length)
             .instruction.length;
 
-        this.helpString = instructions
+        this.helpString = this.instructions
             .map(x => ` ${x.instruction.padEnd(this.instructionPaddingEnd)} ${x.description}`)
             .join('\n');
     }
 
-    private NewLineFormatter(str: string): string {
+    private NewLineFormatter(str: string): AutoWordWrapResult {
         const fontGlyphCollection = this.fontGlyphCollectionFactory(
             {
                 ASCIIRange: { Start: ' ', End: '~' },
@@ -85,26 +96,31 @@ export class LanguageSyntaxHelpControls {
         const strArr = [...str];
         const padding = 20 * this.group.Scale;
 
+        let instructionIdx = 1;
+        const instructionRange: [number, number][] = [[1, 1 + this.instructions[0].instruction.length]];
+
         for (let n = 0, width = padding; n < strArr.length; ++n) {
             const symbol = strArr[n];
 
             if (symbol === '\n') {
                 width = padding;
+                instructionRange.push([n + 2, n + 2 + this.instructions[instructionIdx++].instruction.length]);
                 continue;
             }
 
             const glyph = ExceptionTrap
                 .Try(SelfBind(fontGlyphCollection, 'Lookup'), symbol)
                 .CatchFn(SelfBind(fontGlyphCollection, 'Lookup'), '?');
+
             if (width + glyph.width > this.display!.Dimension.width) {
                 width = padding;
-                strArr.splice(n, 0, ...['\n', ' '.repeat(this.instructionPaddingEnd + 2)]);
+                strArr.splice(n, 0, '\n', ...' '.repeat(this.instructionPaddingEnd + 2));
             } else {
                 width += glyph.width;
             }
         }
 
-        return strArr.join('');
+        return { text: strArr.join(''), instructionRange };
     }
 
     ToggleHelp(): void {
@@ -120,7 +136,10 @@ export class LanguageSyntaxHelpControls {
                 this.lineHeight,
                 this.group);
 
-            this.display.Text = this.NewLineFormatter(this.helpString)
+            const formatedHelp = this.NewLineFormatter(this.helpString);
+
+            this.display.Text = formatedHelp.text;
+            formatedHelp.instructionRange.forEach(x => this.display!.StyleRange(x[0], x[1], { color: [0.9058823529411765, 0.2980392156862745, 0.23529411764705882] }));
         } else {
             this.display.Destroy();
             this.display = null;
